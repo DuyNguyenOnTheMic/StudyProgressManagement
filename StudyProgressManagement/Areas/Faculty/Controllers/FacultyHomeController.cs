fec5aafe1622313d4b995ca3698285127ba1ded6
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-06-16 22:02:16 +0700

    Set logo VLU for faculty page

---
+++ StudyProgressManagement/Areas/Faculty/Controllers/FacultyHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyProgressManagement.Areas.Faculty.Controllers
+{
+    public class FacultyHomeController : Controller
+    {
+        // GET: Faculty/FacultyHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

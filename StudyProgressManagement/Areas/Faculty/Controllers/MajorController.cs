3b2630f1ba8a0aadd89c3edfd3759844e8def21d
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-06-17 22:19:26 +0700

    Create interface and code view list of majors, search majors

---
+++ StudyProgressManagement/Areas/Faculty/Controllers/MajorController.cs
@@ -0,0 +1,27 @@
+﻿using StudyProgressManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyProgressManagement.Areas.Faculty.Controllers
+{
+    [Authorize(Roles = "Faculty")]
+    public class MajorController : Controller
+    {
+        sep_team03Entities db = new sep_team03Entities();
+
+        // GET: Faculty/Major
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            List<major> majorList = db.majors.ToList();
+            return Json(new { data = majorList },JsonRequestBehavior.AllowGet);
+        }
+    }
+}

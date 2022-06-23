0f5316d1f60b0fce47372423a5dda54f37722824
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-06-23 20:34:52 +0700

    Build after clone

--- StudyProgressManagement/Areas/Faculty/Controllers/StudyProgramController.cs
+++ StudyProgressManagement/Areas/Faculty/Controllers/StudyProgramController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace StudyProgressManagement.Areas.Faculty.Controllers
 {
+    [Authorize(Roles = "Faculty")]
     public class StudyProgramController : Controller
     {
         // GET: Faculty/StudyProgram

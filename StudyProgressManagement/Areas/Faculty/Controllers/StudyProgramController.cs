7e8947ca7ff3013adff593dbbb3a76b296daded2
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-06-24 21:08:05 +0700

    Add controller for importing study program

--- StudyProgressManagement/Areas/Faculty/Controllers/StudyProgramController.cs
+++ StudyProgressManagement/Areas/Faculty/Controllers/StudyProgramController.cs
@@ -1,4 +1,8 @@
 ﻿using StudyProgressManagement.Models;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -39,10 +43,70 @@ namespace StudyProgressManagement.Areas.Faculty.Controllers
             var postedStudentCourse = Request.Form["student_course"].ToString();
             foreach (var imageFile in Request.Files)
             {
+                string filePath = string.Empty;
+                if (postedFile != null)
+                {
+                    string path = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
 
+                    filePath = path + Path.GetFileName(postedFile.FileName);
+                    string extension = Path.GetExtension(postedFile.FileName);
+                    postedFile.SaveAs(filePath);
+
+                    string conString = string.Empty;
+                    switch (extension)
+                    {
+                        case ".xls": //Excel 97-03.
+                            conString = ConfigurationManager.ConnectionStrings["Excel03ConString"].ConnectionString;
+                            break;
+                        case ".xlsx": //Excel 07 and above.
+                            conString = ConfigurationManager.ConnectionStrings["Excel07ConString"].ConnectionString;
+                            break;
+                    }
+
+                    DataTable dt = new DataTable();
+                    conString = string.Format(conString, filePath);
+
+                    using (OleDbConnection connExcel = new OleDbConnection(conString))
+                    {
+                        using (OleDbCommand cmdExcel = new OleDbCommand())
+                        {
+                            using (OleDbDataAdapter odaExcel = new OleDbDataAdapter())
+                            {
+                                cmdExcel.Connection = connExcel;
+
+                                //Get the name of First Sheet.
+                                connExcel.Open();
+                                DataTable dtExcelSchema;
+                                dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                                string sheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+                                connExcel.Close();
+
+                                //Read Data from First Sheet.
+                                connExcel.Open();
+                                cmdExcel.CommandText = "SELECT * From [" + sheetName + "]";
+                                odaExcel.SelectCommand = cmdExcel;
+                                odaExcel.Fill(dt);
+                                connExcel.Close();
+                            }
+                        }
+                    }
+
+                    //Insert records to database table.
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        db.curricula.Add(new curriculum
+                        {
+                            id = row["Name"].ToString(),
+                        });
+                    }
+                    db.SaveChanges();
+                }
             }
-
-            return Json(new { status = true, Message = "Account created." + postedMajor + postedStudentCourse });
+            return View();
         }
     }
 }

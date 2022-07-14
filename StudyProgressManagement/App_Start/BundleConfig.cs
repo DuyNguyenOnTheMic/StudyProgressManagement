c402ff043c88daaa329b23023f46db566220248e
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-07-14 13:47:16 +0700

    Complete search student study results
    Related Work Items: #943

--- StudyProgressManagement/App_Start/BundleConfig.cs
+++ StudyProgressManagement/App_Start/BundleConfig.cs
@@ -23,8 +23,7 @@ namespace StudyProgressManagement
                 "~/app-assets/vendors/js/sweetalert2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/sidebarJs").Include(
-               "~/app-assets/js/app-sidebar.min.js",
-               "~/app-assets/js/notification-sidebar.js"));
+               "~/app-assets/js/app-sidebar.min.js"));
 
             // Style Bundles
             bundles.Add(new StyleBundle("~/Content/css").Include(

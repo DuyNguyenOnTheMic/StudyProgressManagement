cd508e3df6785e2304b05a1ea8e7f636c6d5bc5a
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-07-15 20:04:12 +0700

    Add Area for Student role

---
+++ StudyProgressManagement/Areas/Student/Controllers/LearningProgressController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyProgressManagement.Areas.Student.Controllers
+{
+    public class LearningProgressController : Controller
+    {
+        // GET: Student/LearningProgress
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

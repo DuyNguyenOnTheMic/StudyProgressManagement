53cff3ffcced7149a627154f47ed68146282aeb1
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-07-16 16:05:42 +0700

    Update view learning progress of student
    Related Work Items: #946

--- StudyProgressManagement/Areas/Student/Controllers/LearningProgressController.cs
+++ StudyProgressManagement/Areas/Student/Controllers/LearningProgressController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudyProgressManagement.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,83 @@ namespace StudyProgressManagement.Areas.Student.Controllers
     [Authorize]
     public class LearningProgressController : Controller
     {
+        SEP25Team03Entities db = new SEP25Team03Entities();
+
         // GET: Student/LearningProgress
         public ActionResult Index()
         {
+            CheckStudentId();
+
             return View();
+        }
+
+        [HttpPost]
+        public string GetStudentInfo(string studentId)
+        {
+            // Get student information
+            var query_student = db.students.Where(s => s.id == studentId).FirstOrDefault();
+            if (query_student != null)
+            {
+                var studentInfo = query_student.full_name + " - " + studentId + " - " + query_student.student_course.course + " Ngành " + query_student.student_course.major.name;
+                return studentInfo;
+            }
+            return null;
+        }
+
+        [HttpPost]
+        public JsonResult GetData(string studentId)
+        {
+            var query_student = db.students.Where(s => s.id == studentId).FirstOrDefault();
+            var query_studyResult = db.study_results;
+            // Get study results of student
+            if (query_student != null)
+            {
+                return Json(db.curricula.Where(s => s.student_course_id == query_student.student_course_id).Select(s => new
+                {
+                    curriculum_id = s.curriculum_id,
+                    curriculum_name = s.name,
+                    credits = s.credits,
+                    theoretical_hours = s.theoretical_hours,
+                    practice_hours = s.practice_hours,
+                    internship_hours = s.internship_hours,
+                    project_hours = s.project_hours,
+                    compulsory_or_optional = s.compulsory_or_optional,
+                    knowledge_type_group_1 = s.knowledge_type.group_1,
+                    knowledge_type_group_2 = s.knowledge_type.group_2,
+                    knowledge_type_group_3 = s.knowledge_type.group_3,
+                    compulsory_credits = s.knowledge_type.compulsory_credits,
+                    optional_credits = s.knowledge_type.optional_credits,
+                    mark10 = query_studyResult.Where(d => d.student_id == studentId && d.curriculum_id == s.id)
+                    .Select(d => d.mark10).FirstOrDefault().ToString(),
+                    mark10_2 = query_studyResult.Where(d => d.student_id == studentId && d.curriculum_id == s.id)
+                    .Select(d => d.mark10_2).FirstOrDefault().ToString(),
+                    max_mark_10 = query_studyResult.Where(d => d.student_id == studentId && d.curriculum_id == s.id)
+                    .Select(d => d.max_mark_10).FirstOrDefault().ToString(),
+                    max_mark_letter = query_studyResult.Where(d => d.student_id == studentId && d.curriculum_id == s.id)
+                    .Select(d => d.max_mark_letter).FirstOrDefault().ToString(),
+                    is_pass = query_studyResult.Where(d => d.student_id == studentId && d.curriculum_id == s.id)
+                    .Select(d => d.is_pass).FirstOrDefault().ToString()
+
+                }).ToList(), JsonRequestBehavior.AllowGet);
+            }
+            return Json(new { error = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        public void CheckStudentId()
+        {
+            // Get studentId from email
+            string studentEmail = User.Identity.Name;
+            int pFrom = studentEmail.IndexOf(".") + 1;
+            int pTo = studentEmail.LastIndexOf("@");
+
+            string studentId = studentEmail.Substring(pFrom, pTo - pFrom);
+
+            // Check if student has in database
+            var query_student = db.students.Where(s => s.id == studentId).FirstOrDefault();
+            if (query_student != null)
+            {
+                ViewBag.StudentID = studentId;
+            }
         }
     }
 }

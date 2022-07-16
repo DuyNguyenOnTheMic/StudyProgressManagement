fdf32667ee2881bcd5cd1cdfe8682b8880bee1f5
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-07-16 18:09:00 +0700

    Create statistics view for Faculty

---
+++ StudyProgressManagement/Areas/Faculty/Controllers/ProgressStatisticsController.cs
@@ -0,0 +1,19 @@
+﻿using StudyProgressManagement.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace StudyProgressManagement.Areas.Faculty.Controllers
+{
+    [Authorize(Roles = "Faculty")]
+    public class ProgressStatisticsController : Controller
+    {
+        SEP25Team03Entities db = new SEP25Team03Entities();
+
+        // GET: Faculty/ProgressStatistics
+        public ActionResult Index()
+        {
+            ViewBag.majors = db.majors.ToList();
+            return View();
+        }
+    }
+}

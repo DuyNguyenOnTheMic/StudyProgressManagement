551360e7ba61b2faeaa3a31d8a19517c7f12641e
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-07-16 22:20:22 +0700

    Update chartjs for View statistics about student progress
    Related Work Items: #1094

--- StudyProgressManagement/Areas/Faculty/Controllers/ProgressStatisticsController.cs
+++ StudyProgressManagement/Areas/Faculty/Controllers/ProgressStatisticsController.cs
@@ -15,5 +15,41 @@ namespace StudyProgressManagement.Areas.Faculty.Controllers
             ViewBag.majors = db.majors.ToList();
             return View();
         }
+
+        public JsonResult LoadStudentCourses(string majorId)
+        {
+            // get student courses data from database
+            return Json(db.student_course.Where(s => s.major_id == majorId).Select(s => new
+            {
+                id = s.id,
+                course = s.course
+            }).ToList(), JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult GetStatistics(int studentCourseId, int credits)
+        {
+            int passStudents = 0, failStudents = 0;
+
+            // Get study results group by student
+            var query_studyResult = db.study_results.Where(s => s.student_course_id == studentCourseId && s.is_pass != null)
+                .GroupBy(s => s.student_id).Select(s => new { Id = s.Key, Sum = s.Sum(item => item.curriculum.credits) }).ToList();
+
+            foreach (var result in query_studyResult)
+            {
+                // Check if students's credits pass
+                if (result.Sum >= credits)
+                {
+                    passStudents++;
+                }
+                else
+                {
+                    failStudents++;
+                }
+            }
+
+            var finalResult = Json(new { passStudents = passStudents, failStudents = failStudents });
+            return Json(finalResult, JsonRequestBehavior.AllowGet);
+        }
     }
 }

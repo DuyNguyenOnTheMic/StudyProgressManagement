399ff0d1d8edc99cfc942bf45430154b222b3694
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-07-25 09:46:50 +0700

    Add view statistics about learning progress for student interface

---
+++ StudyProgressManagement/Areas/Student/Controllers/StatisticsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudyProgressManagement.Areas.Student.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        // GET: Student/Statistics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}

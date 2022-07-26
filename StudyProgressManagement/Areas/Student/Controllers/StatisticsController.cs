6028ca05ed07df6f72f44b03c7822d04f214d493
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-07-26 10:47:06 +0700

    View statistics base on knowledge type (instead of study results)
    Related Work Items: #1152

--- StudyProgressManagement/Areas/Student/Controllers/StatisticsController.cs
+++ StudyProgressManagement/Areas/Student/Controllers/StatisticsController.cs
@@ -17,23 +17,23 @@ namespace StudyProgressManagement.Areas.Student.Controllers
         [HttpPost]
         public JsonResult GetStatistics()
         {
-            // Get student id
+            // Declare variables
             string studentId = Session["StudentId"].ToString();
             int studentCourseId = int.Parse(Session["StudentCourseId"].ToString());
+            var query_studyResult = db.study_results.Where(s => s.student_id == studentId && s.is_pass != null);
 
             // Get credits count for each knowledge type based on study result
-            var query_studyResult = db.study_results.Where(s => s.student_id == studentId)
-            .GroupBy(s => s.curriculum.knowledge_type.knowledge_type_alias).Select(s => new
+            var query_knowledge = db.knowledge_type.Where(k => k.student_course_id == studentCourseId).GroupBy(k => k.knowledge_type_alias).Select(k => new
             {
-                id = s.Key,
-                group_2 = s.Select(k => k.curriculum.knowledge_type.group_2).FirstOrDefault(),
-                group_3 = s.Select(k => k.curriculum.knowledge_type.group_3).FirstOrDefault(),
-                compulsory_credits = s.Select(k => k.curriculum.knowledge_type.compulsory_credits).FirstOrDefault(),
-                optional_credits = s.Select(k => k.curriculum.knowledge_type.optional_credits).FirstOrDefault(),
-                sum = s.Where(item => item.is_pass != null).Select(item => item.curriculum.credits).DefaultIfEmpty(0).Sum()
+                id = k.Key,
+                group_2 = k.Select(item => item.group_2).FirstOrDefault(),
+                group_3 = k.Select(item => item.group_3).FirstOrDefault(),
+                compulsory_credits = k.Select(item => item.compulsory_credits).FirstOrDefault(),
+                optional_credits = k.Select(item => item.optional_credits).FirstOrDefault(),
+                sum = query_studyResult.Where(item => item.curriculum.knowledge_type.knowledge_type_alias == k.Key).Select(item => item.curriculum.credits).DefaultIfEmpty(0).Sum()
             });
 
-            return Json(query_studyResult.ToList(), JsonRequestBehavior.AllowGet);
+            return Json(query_knowledge.ToList(), JsonRequestBehavior.AllowGet);
         }
     }
 }

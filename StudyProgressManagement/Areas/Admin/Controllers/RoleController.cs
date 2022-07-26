fcb55ee62cd116cb7026d0b8cc0d6ccca2b4b6c3
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-07-26 20:49:51 +0700

    Update popup form for user to edit role
    Related Work Items: #1186

--- StudyProgressManagement/Areas/Admin/Controllers/RoleController.cs
+++ StudyProgressManagement/Areas/Admin/Controllers/RoleController.cs
@@ -19,13 +19,33 @@ namespace StudyProgressManagement.Areas.Admin.Controllers
         public JsonResult GetData()
         {
             // Get majors data from datatabse
-            return Json(db.AspNetUsers.Select(m => new
+            return Json(db.AspNetUsers.Select(u => new
             {
-                id = m.Id,
-                email = m.Email,
-                role = m.AspNetRoles.Select(item => item.Name),
+                id = u.Id,
+                email = u.Email,
+                role = u.AspNetRoles.Select(item => item.Name),
 
             }).ToList(), JsonRequestBehavior.AllowGet);
         }
+
+        [HttpGet]
+        public ActionResult Edit(string id)
+        {
+            // Get user role
+            var query_role = db.AspNetUsers.Find(id);
+            var role_id = query_role.AspNetRoles.Select(item => item.Id).FirstOrDefault();
+
+            ViewBag.role_id = new SelectList(db.AspNetRoles, "id", "name", role_id);
+            return View(db.AspNetUsers.Where(x => x.Id == id).FirstOrDefault());
+        }
+
+        /*[HttpPost]
+        public ActionResult Edit(major major)
+        {
+            // Update major
+            db.Entry(major).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            return Json(new { success = true, message = "Cập nhật thành công!" }, JsonRequestBehavior.AllowGet);
+        }*/
     }
 }

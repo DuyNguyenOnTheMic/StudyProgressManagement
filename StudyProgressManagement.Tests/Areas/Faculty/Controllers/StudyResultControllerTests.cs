e86bf38366417f5fd5fc28ed14d6d3ef2b88aaa7
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-08-02 14:51:01 +0700

    Add study results test controller

---
+++ StudyProgressManagement.Tests/Areas/Faculty/Controllers/StudyResultControllerTests.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudyProgressManagement.Areas.Faculty.Controllers.Tests
+{
+    [TestClass()]
+    public class StudyResultControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SearchTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetStudentInfoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadStudentCoursesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void LoadClassStudentsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ImportTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ImportTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ValidateColumnsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ContainColumnTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DataTableToJsonTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetNullOnEmptyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}

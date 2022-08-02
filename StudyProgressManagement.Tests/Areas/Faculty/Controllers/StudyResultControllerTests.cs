0160130af8a2636007f955041d1aff019eb5a0be
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-08-02 21:08:46 +0700

    Update study results's unit test

--- StudyProgressManagement.Tests/Areas/Faculty/Controllers/StudyResultControllerTests.cs
+++ StudyProgressManagement.Tests/Areas/Faculty/Controllers/StudyResultControllerTests.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyProgressManagement.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace StudyProgressManagement.Areas.Faculty.Controllers.Tests
 {
@@ -6,87 +10,302 @@ namespace StudyProgressManagement.Areas.Faculty.Controllers.Tests
     public class StudyResultControllerTests
     {
         [TestMethod()]
-        public void IndexTest()
+        public void Index_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
         }
 
         [TestMethod()]
-        public void SearchTest()
+        public void Search_View_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            ViewResult result = controller.Search() as ViewResult;
+            var major = new SelectList(db.majors);
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(major.Count(), ((IEnumerable<dynamic>)result.ViewBag.majors).Count());
         }
 
         [TestMethod()]
-        public void SearchTest1()
+        public void Get_Search_Study_Result_Json_Data_With_Class_Not_Null_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, query_classStudent.id, "");
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            foreach (dynamic json in jsonCollection)
+            {
+                Assert.IsNotNull(json.id);
+                Assert.IsNotNull(json.full_name);
+            }
         }
 
         [TestMethod()]
-        public void GetStudentInfoTest()
+        public void Get_Search_Study_Result_Json_Data_With_Class_Correctly_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, query_classStudent.id, "");
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            foreach (dynamic json in jsonCollection)
+            {
+                Assert.IsNotNull(json.id,
+                    "JSON record does not contain \"id\" required property.");
+                Assert.IsNotNull(json.full_name,
+                    "JSON record does not contain \"full_name\" required property.");
+            }
         }
 
         [TestMethod()]
-        public void GetDataTest()
+        public void Search_Study_Result_Json_Data_With_Class_Should_Convert_To_IEnumerable_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, query_classStudent.id, "");
+            dynamic jsonCollection = actionResult.Data;
+            int count = 0;
+            foreach (var value in jsonCollection)
+            {
+                count++;
+            }
+
+            // Assert
+            Assert.IsTrue(count > 0);
+        }
+
+        [TestMethod]
+        public void Search_Study_Result_Json_Data_With_Class_Index_at_0_Should_Not_Be_Null_Test()
+        {
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, query_classStudent.id, "");
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert                
+            Assert.IsNotNull(jsonCollection[0]);
+        }
+
+        [TestMethod]
+        public void Search_Study_Result_JSon_Data_With_Class_Should_Be_Indexable_Test()
+        {
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, query_classStudent.id, "");
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            for (var i = 0; i < jsonCollection.Count; i++)
+            {
+
+                var json = jsonCollection[i];
+
+                Assert.IsNotNull(json);
+                Assert.IsNotNull(json.id,
+                    "JSON record does not contain \"id\" required property.");
+                Assert.IsNotNull(json.full_name,
+                    "JSON record does not contain \"full_name\" required property.");
+            }
         }
 
         [TestMethod()]
-        public void LoadStudentCoursesTest()
+        public void Search_Study_Result_JSon_Data_With_Class_Count_Should_Be_Equal_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var query_count = db.students.Where(s => s.student_course_id == query_studentCourse.id &&
+                s.class_student_id == query_classStudent.id && s.full_name.Contains(""));
+
+            var actionResult = controller.Search(query_studentCourse.id, query_classStudent.id, "");
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.AreEqual(jsonCollection.Count, query_count.Count());
         }
 
         [TestMethod()]
-        public void LoadClassStudentsTest()
+        public void Get_Search_Study_Result_Json_Data_With_Name_Not_Null_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var query_student = db.students.Where(c => c.student_course_id == query_studentCourse.id && c.class_student_id == query_classStudent.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, "", query_student.full_name);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            foreach (dynamic json in jsonCollection)
+            {
+                Assert.IsNotNull(json.id);
+                Assert.IsNotNull(json.full_name);
+            }
         }
 
         [TestMethod()]
-        public void ImportTest()
+        public void Get_Search_Study_Result_Json_Data_With_Name_Correctly_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var query_student = db.students.Where(c => c.student_course_id == query_studentCourse.id && c.class_student_id == query_classStudent.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, "", query_student.full_name);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            foreach (dynamic json in jsonCollection)
+            {
+                Assert.IsNotNull(json.id,
+                    "JSON record does not contain \"id\" required property.");
+                Assert.IsNotNull(json.full_name,
+                    "JSON record does not contain \"full_name\" required property.");
+            }
         }
 
         [TestMethod()]
-        public void ImportTest1()
+        public void Search_Study_Result_Json_Data_With_Name_Should_Convert_To_IEnumerable_Test()
         {
-            Assert.Fail();
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var query_student = db.students.Where(c => c.student_course_id == query_studentCourse.id && c.class_student_id == query_classStudent.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, "", query_student.full_name);
+            dynamic jsonCollection = actionResult.Data;
+            int count = 0;
+            foreach (var value in jsonCollection)
+            {
+                count++;
+            }
+
+            // Assert
+            Assert.IsTrue(count > 0);
+        }
+
+        [TestMethod]
+        public void Search_Study_Result_Json_Data_With_Name_Index_at_0_Should_Not_Be_Null_Test()
+        {
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var query_student = db.students.Where(c => c.student_course_id == query_studentCourse.id && c.class_student_id == query_classStudent.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, "", query_student.full_name);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert                
+            Assert.IsNotNull(jsonCollection[0]);
+        }
+
+        [TestMethod]
+        public void Search_Study_Result_JSon_Data_With_Name_Should_Be_Indexable_Test()
+        {
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var query_student = db.students.Where(c => c.student_course_id == query_studentCourse.id && c.class_student_id == query_classStudent.id).FirstOrDefault();
+            var actionResult = controller.Search(query_studentCourse.id, "", query_student.full_name);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            for (var i = 0; i < jsonCollection.Count; i++)
+            {
+
+                var json = jsonCollection[i];
+
+                Assert.IsNotNull(json);
+                Assert.IsNotNull(json.id,
+                    "JSON record does not contain \"id\" required property.");
+                Assert.IsNotNull(json.full_name,
+                    "JSON record does not contain \"full_name\" required property.");
+            }
         }
 
         [TestMethod()]
-        public void ValidateColumnsTest()
+        public void Search_Study_Result_JSon_Data_With_Name_Count_Should_Be_Equal_Test()
         {
-            Assert.Fail();
-        }
+            // Arrange
+            var controller = new StudyResultController();
+            var db = new SEP25Team03Entities();
 
-        [TestMethod()]
-        public void ContainColumnTest()
-        {
-            Assert.Fail();
-        }
+            // Act
+            var query_studentCourse = db.student_course.FirstOrDefault();
+            var query_classStudent = db.class_student.Where(c => c.student_course_id == query_studentCourse.id).FirstOrDefault();
+            var query_student = db.students.Where(c => c.student_course_id == query_studentCourse.id && c.class_student_id == query_classStudent.id).FirstOrDefault();
+            var query_count = db.students.Where(s => s.student_course_id == query_studentCourse.id &&
+                s.full_name.Contains(query_student.full_name));
 
-        [TestMethod()]
-        public void DataTableToJsonTest()
-        {
-            Assert.Fail();
-        }
+            var actionResult = controller.Search(query_studentCourse.id, "", query_student.full_name);
+            dynamic jsonCollection = actionResult.Data;
 
-        [TestMethod()]
-        public void DeleteTest()
-        {
-            Assert.Fail();
-        }
-
-        [TestMethod()]
-        public void SetNullOnEmptyTest()
-        {
-            Assert.Fail();
+            // Assert
+            Assert.AreEqual(jsonCollection.Count, query_count.Count());
         }
     }
 }

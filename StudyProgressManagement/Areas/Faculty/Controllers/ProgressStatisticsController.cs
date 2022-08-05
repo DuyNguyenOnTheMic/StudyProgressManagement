1d1474052d1090bff6a1053ef34f8e22a17bc546
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-08-05 19:41:29 +0700

    Authorize for Progress Statistics

--- StudyProgressManagement/Areas/Faculty/Controllers/ProgressStatisticsController.cs
+++ StudyProgressManagement/Areas/Faculty/Controllers/ProgressStatisticsController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 
 namespace StudyProgressManagement.Areas.Faculty.Controllers
 {
+    [Authorize(Roles = "Faculty")]
     public class ProgressStatisticsController : Controller
     {
         SEP25Team03Entities db = new SEP25Team03Entities();

db081c15c9e4aa39fe9d109cb2325f671a69588e
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-08-07 15:49:16 +0700

    Update code in student statistics

---
+++ StudyProgressManagement.Tests/Areas/Student/Controllers/StatisticsControllerTests.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyProgressManagement.Areas.Student.Controllers;
+using StudyProgressManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace StudyProgressManagement.Areas.Student.Controllers.Tests
+{
+    [TestClass()]
+    public class StatisticsControllerTests
+    {
+        [TestMethod()]
+        public void Index_Test()
+        {
+            // Arrange
+            var controller = new StatisticsController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void Get_Statistics_Json_Data_Not_Null_Test()
+        {
+            // Arrange
+            var controller = new StatisticsController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_student = db.students.FirstOrDefault();
+            var actionResult = controller.GetStatistics(query_student.id, query_student.student_course_id);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            foreach (dynamic json in jsonCollection)
+            {
+                Assert.IsNotNull(json.id);
+                Assert.IsNotNull(json.knowledge_type_id);
+                Assert.IsNotNull(json.sum);
+            }
+        }
+
+        [TestMethod()]
+        public void Get_Statistics_Json_Data_Correctly_Test()
+        {
+            // Arrange
+            var controller = new StatisticsController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_student = db.students.FirstOrDefault();
+            var actionResult = controller.GetStatistics(query_student.id, query_student.student_course_id);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.IsNotNull(actionResult, "No ActionResult returned from action method.");
+            foreach (dynamic json in jsonCollection)
+            {
+                Assert.IsNotNull(json.id,
+                    "JSON record does not contain \"id\" required property.");
+                Assert.IsNotNull(json.knowledge_type_id,
+                    "JSON record does not contain \"knowledge_type_id\" required property.");
+                Assert.IsNotNull(json.sum,
+                    "JSON record does not contain \"sum\" required property.");
+            }
+        }
+
+        [TestMethod()]
+        public void Statistics_Json_Data_Should_Convert_To_IEnumerable_Test()
+        {
+            // Arrange
+            var controller = new StatisticsController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_student = db.students.FirstOrDefault();
+            var actionResult = controller.GetStatistics(query_student.id, query_student.student_course_id);
+            dynamic jsonCollection = actionResult.Data;
+            int count = 0;
+            foreach (var value in jsonCollection)
+            {
+                count++;
+            }
+
+            // Assert
+            Assert.IsTrue(count > 0);
+        }
+
+        [TestMethod]
+        public void Statistics_Json_Data_Index_at_0_Should_Not_Be_Null_Test()
+        {
+            // Arrange
+            var controller = new StatisticsController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_student = db.students.FirstOrDefault();
+            var actionResult = controller.GetStatistics(query_student.id, query_student.student_course_id);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert                
+            Assert.IsNotNull(jsonCollection[0]);
+        }
+
+        [TestMethod]
+        public void Statistics_JSon_Data_Should_Be_Indexable_Test()
+        {
+            // Arrange
+            var controller = new StatisticsController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_student = db.students.FirstOrDefault();
+            var actionResult = controller.GetStatistics(query_student.id, query_student.student_course_id);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            for (var i = 0; i < jsonCollection.Count; i++)
+            {
+
+                var json = jsonCollection[i];
+
+                Assert.IsNotNull(json);
+                Assert.IsNotNull(json.id,
+                    "JSON record does not contain \"id\" required property.");
+                Assert.IsNotNull(json.knowledge_type_id,
+                    "JSON record does not contain \"knowledge_type_id\" required property.");
+                Assert.IsNotNull(json.sum,
+                    "JSON record does not contain \"sum\" required property.");
+            }
+        }
+
+        [TestMethod()]
+        public void Statistics_JSon_Data_Count_Should_Be_Equal_Test()
+        {
+            // Arrange
+            var controller = new StatisticsController();
+            var db = new SEP25Team03Entities();
+
+            // Act
+            var query_student = db.students.FirstOrDefault();
+            var actionResult = controller.GetStatistics(query_student.id, query_student.student_course_id);
+            var query_knowledge = db.knowledge_type.Where(k => k.student_course_id == query_student.student_course_id).GroupBy(k => k.knowledge_type_alias);
+            dynamic jsonCollection = actionResult.Data;
+
+            // Assert
+            Assert.AreEqual(jsonCollection.Count, query_knowledge.Count());
+        }
+    }
+}
--- StudyProgressManagement/Areas/Student/Controllers/StatisticsController.cs
+++ StudyProgressManagement/Areas/Student/Controllers/StatisticsController.cs
@@ -24,16 +24,15 @@ namespace StudyProgressManagement.Areas.Student.Controllers
             var query_studyResult = db.study_results.Where(s => s.student_id == studentId && s.is_pass != null);
 
             // Get credits count for each knowledge type based on study result
-            var query_knowledge = db.knowledge_type.Where(k => k.student_course_id == studentCourseId).GroupBy(k => k.knowledge_type_alias).Select(k => new
+            var query_knowledge = db.knowledge_type.Where(k => k.student_course_id == studentCourseId).Select(k => new
             {
-                id = k.Key,
-                knowledge_type_id = k.Select(item => item.id).FirstOrDefault(),
-                group_2 = k.Select(item => item.group_2).FirstOrDefault(),
-                group_3 = k.Select(item => item.group_3).FirstOrDefault(),
-                compulsory_credits = k.Select(item => item.compulsory_credits).FirstOrDefault(),
-                optional_credits = k.Select(item => item.optional_credits).FirstOrDefault(),
-                sum = query_studyResult.Where(item => item.curriculum.knowledge_type.knowledge_type_alias == k.Key).Select(item => item.curriculum.credits).DefaultIfEmpty(0).Sum()
-            }).OrderBy(k => k.knowledge_type_id);
+                id = k.knowledge_type_alias,
+                group_2 = k.group_2,
+                group_3 = k.group_3,
+                compulsory_credits = k.compulsory_credits,
+                optional_credits = k.optional_credits,
+                sum = query_studyResult.Where(item => item.curriculum.knowledge_type_id == k.id).Select(item => item.curriculum.credits).DefaultIfEmpty(0).Sum()
+            });
 
             return Json(query_knowledge.ToList(), JsonRequestBehavior.AllowGet);
         }

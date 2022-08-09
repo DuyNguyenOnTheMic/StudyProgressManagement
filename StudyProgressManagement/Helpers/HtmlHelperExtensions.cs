adeb8cdaae1f5d7268c059e345f74ab29618a5ad
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-08-09 21:47:42 +0700

    Optimize website by minify render section scripts

---
+++ StudyProgressManagement/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Ajax.Utilities;
+
+namespace System.Web.Mvc
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString JsMinify(
+            this HtmlHelper helper,
+            Func<object, object> markup)
+        {
+            string notMinifiedJs =
+                (markup.DynamicInvoke(helper.ViewContext) ?? "").ToString();
+
+            var minifier = new Minifier();
+            var minifiedJs = minifier.MinifyJavaScript(notMinifiedJs, new CodeSettings
+            {
+                EvalTreatment = EvalTreatment.MakeImmediateSafe,
+                PreserveImportantComments = false
+            });
+            return new MvcHtmlString(minifiedJs);
+        }
+
+    }
+}

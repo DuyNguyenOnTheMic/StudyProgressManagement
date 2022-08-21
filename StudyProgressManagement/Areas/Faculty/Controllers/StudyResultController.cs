08d807a41874103a92d1b041de232cacef87b0b5
197PM21905 - NGUYỄN TÂN DUY - K25T-PM1 <duy.197pm21905@vanlanguni.vn>
2022-08-21 16:14:44 +0700

    Cleanup redundant code when import study results

--- StudyProgressManagement/Areas/Faculty/Controllers/StudyResultController.cs
+++ StudyProgressManagement/Areas/Faculty/Controllers/StudyResultController.cs
@@ -5,7 +5,6 @@ using System;
 using System.Configuration;
 using System.Data;
 using System.Data.OleDb;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -226,30 +225,25 @@ namespace StudyProgressManagement.Areas.Faculty.Controllers
                         // Declare all columns
                         string studentId = row["StudentID"].ToString();
                         string studentName = row["StudentName"].ToString();
-                        string birthDay = row["BirthDay"].ToString();
-                        string birthPlace = row["BirthPlace"].ToString();
+                        /*string birthDay = row["BirthDay"].ToString();
+                        string birthPlace = row["BirthPlace"].ToString();*/
                         string classStudentId = row["ClassStudentID"].ToString();
                         string classStudentName = row["ClassStudentName"].ToString();
-                        string yearStudy = row["YearStudy"].ToString();
-                        string oldTermId = row["TermID"].ToString();
-                        string termName = row["TermName"].ToString();
+                        /*string yearStudy = row["YearStudy"].ToString();*/
                         string curriculumId = row["CurriculumID"].ToString();
-                        string studyUnitId = row["StudyUnitID"].ToString();
-                        string studyUnitAlias = row["StudyUnitAlias"].ToString();
+                        /*string studyUnitId = row["StudyUnitID"].ToString();
+                        string studyUnitAlias = row["StudyUnitAlias"].ToString();*/
                         string curriculumName = row["CurriculumName"].ToString();
                         string credits = row["Credits"].ToString();
                         string mark10 = row["Mark10"].ToString();
                         string mark10_2 = row["Mark10_2"].ToString();
-                        string mark10_3 = row["Mark10_3"].ToString();
+                        /*string mark10_3 = row["Mark10_3"].ToString();
                         string mark10_4 = row["Mark10_4"].ToString();
-                        string mark10_5 = row["Mark10_5"].ToString();
+                        string mark10_5 = row["Mark10_5"].ToString();*/
                         string maxMark10 = row["MaxMark10"].ToString();
                         string maxMark4 = row["maxMark4"].ToString();
                         string maxMarkLetter = row["MaxMarkLetter"].ToString();
                         string isPass = row["IsPass"].ToString();
-
-
-                        var newTermId = "HK" + studyUnitId.Substring(0, 3);
 
                         var query_classstudent = db.class_student.Where(s => s.id == classStudentId).FirstOrDefault();
                         if (query_classstudent == null)
@@ -271,46 +265,14 @@ namespace StudyProgressManagement.Areas.Faculty.Controllers
                             {
                                 id = studentId,
                                 full_name = studentName,
-                                birth_date = DateTime.ParseExact(birthDay, "dd/MM/yyyy", CultureInfo.InvariantCulture),
-                                birth_place = birthPlace,
                                 class_student_id = classStudentId,
                                 student_course_id = studentCourseId
                             });
                             db.SaveChanges();
                         }
-                        else if (query_student.birth_place == null)
-                        {
-                            query_student.birth_place = birthPlace;
-                            db.Entry(query_student).State = System.Data.Entity.EntityState.Modified;
-                            db.SaveChanges();
-                        }
 
-                        var query_term = db.terms.Where(t => t.id == newTermId).FirstOrDefault();
-                        if (query_term == null)
-                        {
-                            // Add term
-                            db.terms.Add(new term
-                            {
-                                id = newTermId,
-                                name = termName
-                            });
-                            db.SaveChanges();
-                        }
-
-                        var query_study_unit = db.study_unit.Where(s => s.id == studyUnitId).FirstOrDefault();
-                        if (query_study_unit == null)
-                        {
-                            // Add study unit
-                            db.study_unit.Add(new study_unit
-                            {
-                                id = studyUnitId,
-                                alias = studyUnitAlias
-                            });
-                            db.SaveChanges();
-                        }
-
-                        var query_curriculum = db.curricula.Where(c => c.curriculum_id ==
-                        curriculumId && c.student_course_id == studentCourseId).FirstOrDefault();
+                        var query_curriculum = db.curricula.Where(c => c.student_course_id == studentCourseId
+                        && c.curriculum_id == curriculumId).FirstOrDefault();
 
                         if (query_curriculum != null)
                         {
@@ -319,17 +281,11 @@ namespace StudyProgressManagement.Areas.Faculty.Controllers
                             {
                                 mark10 = SetNullOnEmpty(mark10),
                                 mark10_2 = SetNullOnEmpty(mark10_2),
-                                mark10_3 = SetNullOnEmpty(mark10_3),
-                                mark10_4 = SetNullOnEmpty(mark10_4),
-                                mark10_5 = SetNullOnEmpty(mark10_5),
                                 max_mark_10 = SetNullOnEmpty(maxMark10),
                                 max_mark_4 = SetNullOnEmpty(maxMark4),
                                 max_mark_letter = SetNullOnEmpty(maxMarkLetter),
                                 is_pass = SetNullOnEmpty(isPass),
-                                year_study = yearStudy,
-                                term_id = newTermId,
                                 curriculum_id = query_curriculum.id,
-                                study_unit_id = studyUnitId,
                                 student_id = studentId,
                                 student_course_id = studentCourseId
                             });
@@ -362,14 +318,10 @@ namespace StudyProgressManagement.Areas.Faculty.Controllers
         public bool ValidateColumns(DataTable dt)
         {
             // Validate all columns in excel file
-            if (ContainColumn("StudentID", dt) && ContainColumn("StudentName", dt) && ContainColumn("BirthDay", dt)
-                && ContainColumn("BirthPlace", dt) && ContainColumn("ClassStudentID", dt) && ContainColumn("ClassStudentName", dt)
-                && ContainColumn("YearStudy", dt) && ContainColumn("TermID", dt) && ContainColumn("TermName", dt)
-                && ContainColumn("CurriculumID", dt) && ContainColumn("StudyUnitID", dt) && ContainColumn("StudyUnitAlias", dt)
-                && ContainColumn("CurriculumName", dt) && ContainColumn("Credits", dt) && ContainColumn("Mark10", dt)
-                && ContainColumn("Mark10_2", dt) && ContainColumn("Mark10_3", dt) && ContainColumn("Mark10_4", dt)
-                && ContainColumn("Mark10_5", dt) && ContainColumn("MaxMark10", dt) && ContainColumn("maxMark4", dt)
-                && ContainColumn("MaxMarkLetter", dt) && ContainColumn("IsPass", dt))
+            if (ContainColumn("StudentID", dt) && ContainColumn("StudentName", dt) && ContainColumn("ClassStudentID", dt)
+                && ContainColumn("ClassStudentName", dt) && ContainColumn("CurriculumID", dt) && ContainColumn("CurriculumName", dt)
+                && ContainColumn("Credits", dt) && ContainColumn("Mark10", dt) && ContainColumn("Mark10_2", dt) && ContainColumn("MaxMark10", dt)
+                && ContainColumn("maxMark4", dt) && ContainColumn("MaxMarkLetter", dt) && ContainColumn("IsPass", dt))
             {
                 return true;
             }
